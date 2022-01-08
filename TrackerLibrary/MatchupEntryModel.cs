bfb27f02819d3f1def59adceefd905d4f77b7be9
czajka88@gmail.com <czajka88@gmail.com>
2022-01-08 15:09:08 +0100

    Data models added.

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one team in matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score of this particular team.
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the matchup that this team came from as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class PersonModel
+    {
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The primary email address of the person.
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// The primary cell phone number of the person.
+        /// </summary>
+        public string CellphoneNumber { get; set; }
+    }
+}
